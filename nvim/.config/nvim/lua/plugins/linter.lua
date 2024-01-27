e47f83c2866c0b705924148f6d222743daa2f6a7
alunturner <alun.turner@googlemail.com>
2024-01-27 17:29:56 +0000

    add linter

---
+++ nvim/.config/nvim/lua/plugins/linter.lua
@@ -0,0 +1,30 @@
+local Linter = {
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+}
+
+Linter.config = function()
+	local lint = require("lint")
+
+	lint.linters_by_ft = {
+		javascript = { "eslint_d" },
+		typescript = { "eslint_d" },
+		javascriptreact = { "eslint_d" },
+		typescriptreact = { "eslint_d" },
+	}
+
+	local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+	vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+		group = lint_augroup,
+		callback = function()
+			lint.try_lint()
+		end,
+	})
+
+	vim.keymap.set("n", "<leader>l", function()
+		lint.try_lint()
+	end)
+end
+
+return { Linter }
