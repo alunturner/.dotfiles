99b3b66180decafdb7e7eadfcc4853ef2a9d9718
alunturner <alun.turner@googlemail.com>
2024-01-27 12:02:32 +0000

    add package manager (lazy)

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,26 @@
-print("hello from simlinked init.lua")
+-- STEP 0 - INSTALL PACKAGE MANAGER AND SET LEADER KEY (SPACE)
+-- Install lazy if not installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Set the leader key before loading plugins
+local leader = " "
+vim.g.mapleader = leader
+vim.g.maplocalleader = leader
+vim.keymap.set({"n","v"}, leader, "<nop>")
+
+-- Use a protected call so we don't error out on first use
+local ok, lazy = pcall(require, "lazy")
+if not ok then
+    return
+end
