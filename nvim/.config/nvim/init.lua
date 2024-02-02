cd82aafd7b287ead9e106d0a6b2899994cad80ba
alunturner <56027671+alunturner@users.noreply.github.com>
2024-02-02 16:39:52 +0000

    Add settings, remaps, tags to treesitter (#2)

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,3 @@
--- STEP 0 - INSTALL PACKAGE MANAGER AND SET LEADER KEY (SPACE)
 -- Install lazy if not installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -21,3 +20,67 @@ vim.keymap.set({ "n", "v" }, leader, "<nop>")
 
 -- STEP 1 - INSTALL EXTERNAL DEPENDENCIES
 require("lazy").setup("plugins")
+
+-- STEP 2 - KEY REMAPS
+local function map(mode, lhs, rhs)
+	vim.keymap.set(mode, lhs, rhs, { silent = true })
+end
+
+-- VSCode style shifting around of lines in visual mode
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Quick way to remove highlighting
+map("n", "<Esc>", "<cmd>nohl<cr>")
+
+-- Keep cursor centred when moving the screen
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+
+-- Window navigation
+map("n", "<Left>", "<C-w>h")
+map("n", "<Down>", "<C-w>j")
+map("n", "<Up>", "<C-w>k")
+map("n", "<Right>", "<C-w>l")
+
+-- When pasting over a selection, maintain the original paste register
+map("v", "p", '"_dP')
+
+-- Make Y act like C and D
+map("n", "Y", "y$")
+
+-- STEP 4 - SETTINGS
+local o = vim.opt
+
+-- Tabs/spaces
+o.expandtab = true
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
+o.smartindent = true
+-- Clipboard
+o.clipboard = "unnamedplus"
+-- Search
+o.ignorecase = true
+o.smartcase = true
+o.showmatch = true
+-- New windows
+o.splitbelow = true
+o.splitright = true
+-- Text display
+o.breakindent = true
+o.scrolloff = 5
+o.sidescrolloff = 5
+o.colorcolumn = "80"
+o.cursorline = true
+o.signcolumn = "yes"
+o.number = true
+-- Other
+o.termguicolors = true
+o.undofile = true
+o.updatetime = 500
+o.timeoutlen = 500
+o.jumpoptions = "stack"
+-- Lines
+o.laststatus = 0
+o.winbar = "%=%m %f"
