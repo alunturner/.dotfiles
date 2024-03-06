2496bb9e14330c6e40e872f3f48786805117ca61
alunturner <art@metrea.aero>
2024-03-06 13:25:04 +0000

    remove Carbon

--- nvim/.config/nvim/lua/plugins/explorer.lua
+++
@@ -1,9 +0,0 @@
-local Explorer = { "SidOfc/carbon.nvim" }
-
-Explorer.config = function()
-	require("carbon").setup()
-
-	vim.keymap.set("n", "<leader>e", "<cmd>Fcarbon<CR>")
-end
-
-return { Explorer }
