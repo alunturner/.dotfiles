8abd42cf5e323127b83a31985e3cfe77a3afd566
alunturner <alun.turner@googlemail.com>
2024-03-11 17:16:40 +0000

    try and add lush to read from pax

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,14 +1,14 @@
 -- Install lazy if not installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -23,7 +23,7 @@ require("lazy").setup("plugins")
 
 -- STEP 2 - KEY REMAPS
 local function map(mode, lhs, rhs)
-    vim.keymap.set(mode, lhs, rhs, { silent = true })
+	vim.keymap.set(mode, lhs, rhs, { silent = true })
 end
 
 -- VSCode style shifting around of lines in visual mode
---
+++ nvim/.config/nvim/lua/plugins/lush.lua
@@ -0,0 +1,6 @@
+local Lush = {
+	"rktjmp/lush.nvim",
+	{ dir = "/home/alunturner/code/pax", lazy = true },
+}
+
+return { Lush }
