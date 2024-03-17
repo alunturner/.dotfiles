4cf02fce948a04e218657cd51cee7d445056e2be
alunturner <alun.turner@googlemail.com>
2024-03-17 20:50:22 +0000

    add comment

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -107,7 +107,7 @@ o.timeoutlen = 500
 o.jumpoptions = "stack"
 -- Lines
 o.laststatus = 0
-o.fillchars = "eob: "
+o.fillchars = "eob: " -- check fillchars as can alter vsplits and hsplits
 o.rulerformat = "%20(%=%m %t%)"
 -- Completion
 o.completeopt = "menu"
