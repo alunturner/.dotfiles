13f4f88424390eeaeb04e3a12b6225c116263456
alunturner <alun.turner@googlemail.com>
2024-03-17 11:19:18 +0000

    copy pax folder across (not a submodule)

---
+++ pax/samples/javascript.jsx
@@ -0,0 +1,71 @@
+const thing = require("./some path");
+import thingy from "some/other/path";
+console.log(thingy);
+
+// We are going to have some comments then
+let usSet = "up a bunche of \n variables";
+const like = true;
+let pi = Math.PI;
+
+const arr = [];
+const a = { object_with: 123 };
+
+const float = 123.456;
+// TODO: - make this work
+// FIXME: 
+// XXX: 
+// Todo - something
+
+let result = float + a.object_with;
+const anItem = document.window.window.window;
+
+if (arr === []) {
+}
+
+class MyClass {
+  constructor() {
+    return this;
+  }
+
+  mew() {
+    console.log(`mew`);
+  }
+}
+
+let _a = null,
+  b = undefined,
+  c = true,
+  d = false,
+  e = Infinity,
+  f = Math.PI,
+    g = NaN;
+
+console.log(a, b, c, d, e, f);
+function thisThingy(num1, num2, num3, word) {}
+
+thisThingy;
+thisThingy(1, 2, 3, "hell");
+thisThingy();
+thisThingy(1),
+let aClassThing = new MyClass();
+aClassThing.mew();
+
+export { result, float };
+
+function one(num1) {}
+const item = () => {};
+const thing = one;
+
+thing();
+
+const item = <div with="some attributes">and some content</div>;
+
+function Component({ id }) {
+  const hrefTextThing = `123466`;
+    
+    
+
+  return <a href={hrefTextthing}>innerText</a>;
+}
+
+const myThing = <Component id={"1234556"} />;
---
+++ pax/samples/lua.lua
@@ -0,0 +1,108 @@
+-- STEP 0 - Install plugin manager (lazy)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Set the leader key before loading plugins
+local function set(mode, lhs, rhs)
+	-- TODO make it so that this util can combine options into the 4th arg
+	vim.keymap.set(mode, lhs, rhs, { silent = true })
+end
+
+local leader = " "
+vim.g.mapleader = leader
+vim.g.maplocalleader = leader
+vim.keymap.set({ "n", "v" }, leader, "<nop>")
+
+-- STEP 1 - INSTALL EXTERNAL DEPENDENCIES
+require("lazy").setup("plugins")
+
+-- STEP 2 - KEY REMAPS
+-- Quick way to remove highlighting
+set("n", "<Esc>", "<cmd>nohl<cr>")
+-- Centre cursor on screen scroll (always)
+set("n", "<C-d>", "<C-d>zz")
+set("n", "<C-u>", "<C-u>zz")
+-- Centre cursor on search (always)
+set("n", "n", "nzz")
+set("n", "N", "Nzz")
+vim.keymap.set("c", "<cr>", function()
+	local cmd = vim.fn.getcmdtype()
+	local is_search = cmd == "/" or cmd == "?"
+	return is_search and "<cr>zz" or "<cr>"
+end, { expr = true })
+-- Centre cursor on entering insert mode (optional)
+set("n", "<leader>i", "zzi")
+set("n", "<leader>I", "zzI")
+set("n", "<leader>a", "zza")
+set("n", "<leader>A", "zzA")
+set("n", "<leader>o", "zzo")
+set("n", "<leader>O", "zzO")
+-- Centre cursor on line jump (nb :LineNr still only moves cursor)
+set("n", "gg", "ggzz")
+-- When pasting over a selection, maintain the original paste register
+set("v", "p", '"_dP')
+-- Make Y act like C and D
+set("n", "Y", "y$")
+-- Don't move the cursor when using J
+set("n", "J", "mzJ`z")
+-- File Browser
+set("n", "<leader>e", "<cmd>Ex<cr>")
+-- Completion - see :h ins-completion
+set("i", "<C-o>", "<C-x><C-o>")
+set("i", "<C-f>", "<C-x><C-f>")
+
+-- STEP 4 - SETTINGS
+local o = vim.opt
+-- Tabs/spaces
+o.expandtab = true
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
+o.smartindent = true
+-- Clipboard
+o.clipboard = "unnamedplus"
+-- Search
+o.ignorecase = true
+o.smartcase = true
+o.showmatch = true
+-- New windows
+o.splitbelow = true
+o.splitright = true
+-- Text display
+o.breakindent = true
+o.colorcolumn = "80"
+o.cursorline = true
+o.cursorcolumn = true
+o.signcolumn = "yes"
+o.number = true
+-- Other
+o.termguicolors = true
+o.undofile = true
+o.swapfile = false
+o.updatetime = 500
+o.timeoutlen = 500
+o.jumpoptions = "stack"
+-- Lines
+o.laststatus = 0
+o.winbar = "%=%m %f"
+o.fillchars = "eob: "
+-- Completion
+o.completeopt = "menu"
+
+-- STEP 5 - COLORSCHEME AND CUSTOMISATION
+vim.cmd("colorscheme pax")
+set("n", "<leader>j", "<cmd>Inspect<cr>")
+vim.diagnostic.config({ virtual_text = false })
+
+-- STEP 6 - ABBREVIATIONS
+vim.cmd("iab tbitd toBeInTheDocument()")
