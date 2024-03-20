4bc1058f7d0ea428ec6895073aac427d431c1cae
alunturner <alun.turner@googlemail.com>
2024-03-20 16:44:55 +0000

    remove treesitter text objects

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,9 +2,6 @@ local Treesitter = {
 	"nvim-treesitter/nvim-treesitter",
 	lazy = false,
 	build = ":TSUpdate",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
-	},
 }
 
 Treesitter.config = function()
@@ -23,29 +20,6 @@ Treesitter.config = function()
 		highlight = {
 			enable = true,
 			additional_vim_regex_highlighting = false,
-		},
-		textobjects = {
-			select = {
-				enable = true,
-				lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-				keymaps = {
-					-- You can use the capture groups defined in textobjects.scm
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["at"] = "@tag.outer",
-					["it"] = "@tag.inner",
-				},
-			},
-			move = {
-				enable = true,
-				set_jumps = true, -- whether to set jumps in the jumplist
-				goto_next_start = {
-					["]f"] = "@function.outer",
-				},
-				goto_previous_start = {
-					["[f"] = "@function.outer",
-				},
-			},
 		},
 	})
 end
