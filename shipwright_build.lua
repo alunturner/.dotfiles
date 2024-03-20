52bfab491affdddfe1ee7f19a0eb9403050357df
alunturner <alun.turner@googlemail.com>
2024-03-20 21:41:47 +0000

    try some new paths

--- shipwright_build.lua
+++ shipwright_build.lua
@@ -2,11 +2,11 @@
 
 local lushwright = require("shipwright.transform.lush")
 run(
-	{},
+	require("pax"),
 	-- generate lua code
 	lushwright.to_lua,
 	-- write the lua code into our destination.
 	-- you must specify open and close markers yourself to account
 	-- for differing comment styles, patchwrite isn't limited to lua files.
-	{ patchwrite, "colors/colorscheme.lua", "-- PATCH_OPEN", "-- PATCH_CLOSE" }
+	{ patchwrite, "colorscheme.lua", "-- PATCH_OPEN", "-- PATCH_CLOSE" }
 )
