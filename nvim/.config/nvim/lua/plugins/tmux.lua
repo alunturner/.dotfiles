9f085aafd0eab09d0c8d53d0cd87cca6426e9c48
alunturner <alun.turner@googlemail.com>
2024-03-27 21:03:42 +0000

    remove unused tmux keybind

--- nvim/.config/nvim/lua/plugins/tmux.lua
+++ nvim/.config/nvim/lua/plugins/tmux.lua
@@ -12,7 +12,6 @@ local TmuxNavigator = {
 		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
 		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
 		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-		{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
 	},
 }
 
