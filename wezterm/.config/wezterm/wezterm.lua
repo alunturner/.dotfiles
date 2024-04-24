31bd6ad58f245c5fa8f0a3c421e48559bb498ca5
alunturner <alun.turner@googlemail.com>
2024-04-24 23:55:48 +0100

    use cyberpunk theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 -- For example, changing the color scheme:
-config.color_scheme = "Batman"
+config.color_scheme = "cyberpunk"
 config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
 -- >>> TODO add colour configuration
 -- https://wezfurlong.org/wezterm/config/appearance.html#defining-your-own-colors
