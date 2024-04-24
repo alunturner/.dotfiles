696ca6be92796ec894351aef58567db4766866f8
alunturner <alun.turner@googlemail.com>
2024-04-24 23:35:48 +0100

    use the right font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,7 +6,8 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 -- For example, changing the color scheme:
-config.color_scheme = "AdventureTime"
+config.color_scheme = "Batman"
+config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
 
 -- and finally, return the configuration to wezterm
 return config
