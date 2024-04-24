a371c9b4f290aee61fa7f9639efc0d71ed443972
alunturner <56027671+alunturner@users.noreply.github.com>
2024-04-24 23:09:45 +0100

    remove the maps that clash with dot repeat (#30)

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -75,17 +75,6 @@ Treesitter.config = function()
 			},
 		},
 	})
-
-	-- Make moves repeatable
-	local repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-	-- vim way: ; goes to the direction you were moving.
-	vim.keymap.set({ "n", "x", "o" }, ";", repeat_move.repeat_last_move)
-	vim.keymap.set({ "n", "x", "o" }, ",", repeat_move.repeat_last_move_opposite)
-	-- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-	vim.keymap.set({ "n", "x", "o" }, "f", repeat_move.builtin_f)
-	vim.keymap.set({ "n", "x", "o" }, "F", repeat_move.builtin_F)
-	vim.keymap.set({ "n", "x", "o" }, "t", repeat_move.builtin_t)
-	vim.keymap.set({ "n", "x", "o" }, "T", repeat_move.builtin_T)
 end
 
 return { Treesitter }
