6fafc5bb1abaa402f72d2d95bf67cb138e5b180c
alunturner <alun.turner@googlemail.com>
2024-04-25 21:30:10 +0100

    first pass of configuration complete

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,15 +1,40 @@
--- Pull in the wezterm API
-local wezterm = require("wezterm")
+local w = require("wezterm")
 
--- This will hold the configuration.
-local config = wezterm.config_builder()
+local config = w.config_builder()
 
--- This is where you actually apply your config choices
--- For example, changing the color scheme:
-config.color_scheme = "cyberpunk"
-config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
--- >>> TODO add colour configuration
--- https://wezfurlong.org/wezterm/config/appearance.html#defining-your-own-colors
+-- COLOR SCHEME
+-- See https://wezfurlong.org/wezterm/config/lua/wezterm/get_builtin_color_schemes.html
+-- for details of how these thees can be altered (like we do the cursor)
+local hibiscus = "hsl:330 100 50"
+local tempWhite = "#e9e7dd"
+local d = w.get_builtin_color_schemes()["Github Dark (Gogh)"]
+d.cursor_bg = hibiscus
+d.foreground = tempWhite
 
--- and finally, return the configuration to wezterm
+local l = w.get_builtin_color_schemes()["Github Light (Gogh)"]
+l.cursor_bg = hibiscus
+
+config.color_schemes = {
+	["light"] = l,
+	["dark"] = d,
+}
+
+config.color_scheme = "dark"
+
+-- FONT
+config.font = w.font("JetBrainsMonoNL Nerd Font")
+config.font_size = 15
+
+-- COMMAND PALETTE
+config.command_palette_font_size = 18
+
+-- OTHER
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_padding = {
+	left = 0,
+	top = 0,
+	right = 0,
+	bottom = 0,
+}
 return config
