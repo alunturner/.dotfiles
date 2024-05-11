f36a21cc4571864358b950d576ace2a7555acd56
alunturner <alun.turner@googlemail.com>
2024-05-11 12:26:00 +0100

    start an init.lua for /colors

---
+++ colors/init.lua
@@ -0,0 +1,7 @@
+local catpuccin_colors = {}
+
+local catpuccin = { dark = {}, light = {} }
+
+local pax_colors = { midnight = "", cream = "", hibiscus = "" }
+
+local pax = { dark = {}, light = {} }
