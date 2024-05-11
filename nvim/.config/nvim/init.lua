10396238323b86c23053d984d66b7c9d68015b68
alunturner <alun.turner@googlemail.com>
2024-05-11 22:01:37 +0100

    change from mono to zero

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -123,7 +123,7 @@ vim.api.nvim_create_autocmd("filetype", {
 
 -- STEP 5 - COLORSCHEME AND CUSTOMISATION
 local _border = "rounded"
-vim.cmd("colorscheme pax_mono")
+vim.cmd("colorscheme pax_zero")
 vim.diagnostic.config({ float = { border = _border }, virtual_text = false })
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 	border = _border,
