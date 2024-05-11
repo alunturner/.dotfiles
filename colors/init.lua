18c501d5a4537e86eb606ef127e3063060e3595e
alunturner <alun.turner@googlemail.com>
2024-05-11 12:30:07 +0100

    use default catpuccin, so don't need to customise

--- colors/init.lua
+++ colors/init.lua
@@ -1,7 +1,3 @@
-local catpuccin_colors = {}
-
-local catpuccin = { dark = {}, light = {} }
-
 local pax_colors = { midnight = "", cream = "", hibiscus = "" }
 
 local pax = { dark = {}, light = {} }
