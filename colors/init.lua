fc8d903f394a7347f46f3eccc07683ccdd6485f2
alunturner <alun.turner@googlemail.com>
2024-05-11 12:37:02 +0100

    add the theme roles

--- colors/init.lua
+++ colors/init.lua
@@ -1,5 +1,36 @@
 local colors = { midnight = "#19191f", cream = "#e9e7dd", hibiscus = "#ff007b" }
 
-local theme = { dark = {}, light = {} }
+local theme = {
+	dark = {
+		bg = "",
+		bg_plus = "",
+		bg_plus_plus = "",
+		mg = "",
+		fg_minus_minus = "",
+		fg_minus = "",
+		fg = "",
+		cursor_bg = "",
+		cursor_fg = "",
+		error = "",
+		warning = "",
+		success = "",
+		higlights = {},
+	},
+	light = {
+		bg = "",
+		bg_plus = "",
+		bg_plus_plus = "",
+		mg = "",
+		fg_minus_minus = "",
+		fg_minus = "",
+		fg = "",
+		cursor_bg = "",
+		cursor_fg = "",
+		error = "",
+		warning = "",
+		success = "",
+		higlights = {},
+	},
+}
 
 return { theme, colors }
