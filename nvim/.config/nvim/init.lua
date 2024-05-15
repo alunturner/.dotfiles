4d9d6bf75057630cafcc3369013ff4b93caeb208
alunturner <alun.turner@googlemail.com>
2024-05-15 22:08:36 +0100

    move experimental stuff to separate file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -23,14 +23,4 @@ require("keybinds")
 require("options")
 require("colorscheme")
 require("abbreviations")
-
--- STEP 7 - EXPERIMENTAL
--- Example error message from tsc
--- src/features/app/components/AppHeader/AppHeader.tsx(11,19): error TS2552: Cannot find name useAccount. Did you mean account?
-local augroup = vim.api.nvim_create_augroup("makers", { clear = true })
--- TODO use the errorformat (see :h errorformat) to try and tidy the qflist
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "typescript,typescriptreact",
-	group = augroup,
-	command = "compiler tsc | setlocal makeprg=tsc",
-})
+require("experimental")
