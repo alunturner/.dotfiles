ca71f86dacb9f155d73796ca539ca3ef86d644a1
alunturner <alun.turner@googlemail.com>
2024-05-15 22:56:59 +0100

    tidy up comments

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,4 @@
--- STEP 0 - Install plugin manager (lazy)
+-- Install plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
