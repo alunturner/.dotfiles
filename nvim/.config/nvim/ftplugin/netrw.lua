215f313edf29087ebbfd5b7cf6ce17b634d235f4
alunturner <56027671+alunturner@users.noreply.github.com>
2024-05-15 22:54:41 +0100

    Move netrw stuff to ftplugin folder (#41)
    * experiment to try and extract netrw specific stuff
    
    * remove the banner by default
    
    * make netrw ftplugin file

---
+++ nvim/.config/nvim/ftplugin/netrw.lua
@@ -0,0 +1,5 @@
+vim.g.netrw_banner = 0
+vim.opt_local.colorcolumn = ""
+vim.keymap.set("n", "<esc>", "<cmd>Rex<cr>", { remap = true, buffer = true })
+vim.keymap.set("n", "h", "-", { remap = true, buffer = true })
+vim.keymap.set("n", "l", "<cr>", { remap = true, buffer = true })
