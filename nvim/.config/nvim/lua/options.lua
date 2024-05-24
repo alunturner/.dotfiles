3cb0cf58f00d9066f5391ddcc6847cc01255ac26
alunturner <alun.turner@googlemail.com>
2024-05-24 22:57:18 +0100

    sort options alphabetically

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,29 +1,29 @@
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.smartindent = true
+vim.o.guicursor = vim.o.guicursor .. ",a:Cursor" -- append hl-Cursor to all modes
+vim.opt.breakindent = true
 vim.opt.clipboard = "unnamedplus"
+vim.opt.colorcolumn = "80"
+vim.opt.completeopt = "longest,menu"
+vim.opt.cursorcolumn = true
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.fillchars = "eob: "
 vim.opt.ignorecase = true
+vim.opt.jumpoptions = "stack"
+vim.opt.laststatus = 0
+vim.opt.number = true
+vim.opt.shiftwidth = 4
+vim.opt.sidescrolloff = 7
+vim.opt.signcolumn = "no"
 vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.softtabstop = 4
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.breakindent = true
-vim.opt.colorcolumn = "80"
-vim.opt.cursorline = true
-vim.opt.cursorcolumn = true
-vim.opt.signcolumn = "no"
-vim.opt.number = true
-vim.opt.wildmode = "longest:full,full"
-vim.o.guicursor = vim.o.guicursor .. ",a:Cursor" -- append hl-Cursor to all modes
-vim.opt.undofile = true
 vim.opt.swapfile = false
-vim.opt.jumpoptions = "stack"
-vim.opt.sidescrolloff = 7
-vim.opt.laststatus = 0
-vim.opt.fillchars = "eob: "
-vim.opt.completeopt = "longest,menu"
--- Custom Ruler
+vim.opt.tabstop = 4
+vim.opt.undofile = true
+vim.opt.wildmode = "longest:full,full"
+
 function GetErrors()
 	local errors = vim.diagnostic.count()[vim.diagnostic.severity.ERROR] or 0
 	return errors > 0 and "%#DiagnosticError# " or ""
