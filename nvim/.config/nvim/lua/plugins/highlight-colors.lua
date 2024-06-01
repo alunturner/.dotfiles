58b2cebd06de95baf477a1e7199a8a6f2d7b1732
alunturner <alun.turner@googlemail.com>
2024-06-01 12:11:32 +0100

    remove event loader from highlight-colors

--- nvim/.config/nvim/lua/plugins/highlight-colors.lua
+++ nvim/.config/nvim/lua/plugins/highlight-colors.lua
@@ -1,7 +1,6 @@
 local HighlightColors = {
 	"brenoprata10/nvim-highlight-colors",
 	version = "*",
-	event = "VeryLazy",
 	config = function()
 		local plugin = require("nvim-highlight-colors")
 		plugin.setup({ enable_tailwind = true })
