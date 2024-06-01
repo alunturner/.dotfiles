6a611b9890a8947ae7b4dc0fc9d08c06d3c73d4e
alunturner <56027671+alunturner@users.noreply.github.com>
2024-06-01 18:51:49 +0100

    Abbreviations (#52)
    * try out abbreviations in /after
    
    * move abbreviations into after/ftplugin/javascript.lua
    
    * add the other ecma filetypes
    
    * don't escape after tbitd

---
+++ nvim/.config/nvim/after/ftplugin/javascript.lua
@@ -0,0 +1,2 @@
+vim.cmd("iab <buffer> tbitd toBeInTheDocument()")
+vim.cmd("iab <buffer> fna () => {}")
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -12,13 +12,11 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Set the leader key before loading plugins
+-- Need to set leader before loading plugins
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.keymap.set({ "n", "v" }, " ", "<nop>", { silent = true })
 
--- Load the plugins followed by all local setup
 require("lazy").setup("plugins")
 require("keybinds")
 require("options")
-require("abbreviations")
