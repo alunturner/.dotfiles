8225551eef516b0c661621a3a1580ade53367a17
alunturner <alun.turner@googlemail.com>
2024-06-01 12:28:39 +0100

    refactor formatter to use opts

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -1,26 +1,24 @@
 local Formatter = {
 	"stevearc/conform.nvim",
 	pin = true,
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				javascript = { "prettierd" },
-				typescript = { "prettierd" },
-				javascriptreact = { "prettierd" },
-				typescriptreact = { "prettierd" },
-				css = { "prettierd" },
-				html = { "prettierd" },
-				json = { "prettierd" },
-				yaml = { "prettierd" },
-				markdown = { "prettierd" },
-				lua = { "stylua" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				timeout_ms = 500,
-			},
-		})
-	end,
+	opts = {
+		formatters_by_ft = {
+			javascript = { "prettierd" },
+			typescript = { "prettierd" },
+			javascriptreact = { "prettierd" },
+			typescriptreact = { "prettierd" },
+			css = { "prettierd" },
+			html = { "prettierd" },
+			json = { "prettierd" },
+			yaml = { "prettierd" },
+			markdown = { "prettierd" },
+			lua = { "stylua" },
+		},
+		format_on_save = {
+			lsp_fallback = true,
+			timeout_ms = 500,
+		},
+	},
 }
 
 return { Formatter }
