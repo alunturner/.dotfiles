8abd7ff75ffe9a4b30177015f1e6d231a691d8ee
alunturner <alun.turner@googlemail.com>
2024-06-01 12:10:00 +0100

    update formatter

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -1,27 +1,25 @@
 local Formatter = {
 	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				javascriptreact = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				css = { "prettierd" },
+				html = { "prettierd" },
+				json = { "prettierd" },
+				yaml = { "prettierd" },
+				markdown = { "prettierd" },
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				timeout_ms = 500,
+			},
+		})
+	end,
 }
-
-Formatter.config = function()
-	require("conform").setup({
-		formatters_by_ft = {
-			javascript = { "prettierd" },
-			typescript = { "prettierd" },
-			javascriptreact = { "prettierd" },
-			typescriptreact = { "prettierd" },
-			css = { "prettierd" },
-			html = { "prettierd" },
-			json = { "prettierd" },
-			yaml = { "prettierd" },
-			markdown = { "prettierd" },
-			lua = { "stylua" },
-		},
-		format_on_save = {
-			lsp_fallback = true,
-			timeout_ms = 500,
-		},
-	})
-end
 
 return { Formatter }
