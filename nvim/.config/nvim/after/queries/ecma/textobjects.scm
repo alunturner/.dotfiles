f623089d0bd4b7db3d9fe8250a603ad4f6f1f8f3
alunturner <56027671+alunturner@users.noreply.github.com>
2024-06-02 22:25:30 +0100

    Add custom treesitter queries for ecma (#54)
    * WIP quotes text object
    
    * get string working
    
    * remove the annoying heavy top border - can't hide it on single pane
    
    * get template string working
    
    * udpate comments
    
    * WIP entry syntax
    
    * complete custom entry for objects

--- nvim/.config/nvim/after/queries/ecma/textobjects.scm
+++ nvim/.config/nvim/after/queries/ecma/textobjects.scm
@@ -1,8 +1,10 @@
 ; extends
-; entry
-(array (_) @entry.inner ","? @_end (#make-range! "entry.outer" @entry.inner @_end))
-(object (_) @entry.inner ","? @_end (#make-range! "entry.outer" @entry.inner @_end))
+; entry => e represents an item in anything that is list like
+; array
+(array ((_) @_start @custom_entry.inner . ","? @_end)(#make-range! "custom_entry.outer" @_start @_end))
+; object
+((object (pair key: (_) value: (_)) @_start @custom_entry.inner . ","? @_end)(#make-range! "custom_entry.outer" @_start @_end))
 
-; string
-(string (_) @string.inner) @string.outer
-(template_string (_) @string.inner) @string.outer
+; quotations => q represents anything wrapped in " or ' or `
+(string (["'" "\""] . (_) @_start (_)? @_end . ["'" "\""])(#make-range! "custom_quotation.inner" @_start @_end)) @custom_quotation.outer
+(template_string ("`" . (_) @_start (_)? @_end . "`")(#make-range! "custom_quotation.inner" @_start @_end)) @custom_quotation.outer
