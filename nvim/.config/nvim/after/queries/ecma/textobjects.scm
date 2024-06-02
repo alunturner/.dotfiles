6cf7ebf6b8dea0e267d1563a0d8c5eef22bc107f
alunturner <alun.turner@googlemail.com>
2024-06-02 20:30:11 +0100

    udpate comments

--- nvim/.config/nvim/after/queries/ecma/textobjects.scm
+++ nvim/.config/nvim/after/queries/ecma/textobjects.scm
@@ -1,8 +1,8 @@
 ; extends
-; entry
+; entry => e represents anything that is list like, array item, object item, argument item, parameter item
 (array (_) @entry.inner ","? @_end (#make-range! "entry.outer" @entry.inner @_end))
 (object (_) @entry.inner ","? @_end (#make-range! "entry.outer" @entry.inner @_end))
 
-; string
+; quotations => q represents anything wrapped in " or ' or `
 (string (["'" "\""] . (_) @_start (_)? @_end . ["'" "\""])(#make-range! "custom_quotation.inner" @_start @_end)) @custom_quotation.outer
 (template_string ("`" . (_) @_start (_)? @_end . "`")(#make-range! "custom_quotation.inner" @_start @_end)) @custom_quotation.outer
