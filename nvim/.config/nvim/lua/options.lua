8d29ba742ca4961a13110738b08a0cfbefef8113
alunturner <alun.turner@googlemail.com>
2024-06-03 20:00:57 +0100

    remove all the unused stuff

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -7,6 +7,7 @@ vim.opt.completeopt = "longest,menu"
 vim.opt.cursorcolumn = true
 vim.opt.cursorline = true
 vim.opt.expandtab = true
+vim.opt.fillchars = { eob = " ", wbr = "▀", vert = "█", stlnc = "▀" } -- see unicode block
 vim.opt.ignorecase = true
 vim.opt.jumpoptions = "stack"
 vim.opt.laststatus = 0
@@ -24,6 +25,7 @@ vim.opt.swapfile = false
 vim.opt.tabstop = 4
 vim.opt.undofile = true
 vim.opt.wildmode = "longest:full,full"
+vim.opt.winbar = "%=▜ %{%v:lua.GetModifiedFlag()%} %t ▛%=" -- see unicode quadrant
 
 vim.cmd("colorscheme pax_zero")
 
@@ -34,40 +36,6 @@ vim.diagnostic.config({
 	jump = { float = true }, -- see https://github.com/neovim/neovim/pull/29067
 })
 
-function GetErrorFlag()
-	local errors = vim.diagnostic.count(0)[vim.diagnostic.severity.ERROR] or 0
-	return errors > 0 and "%#DiagnosticError#" or " "
-end
 function GetModifiedFlag()
-	local icon = vim.bo.modified and "" or ""
-	return icon
+	return vim.bo.modified and "" or ""
 end
-
-vim.opt.winbar = "%=▜ %{%v:lua.GetModifiedFlag()%} %t ▛%="
-vim.opt.fillchars = { eob = " ", wbr = "▀", vert = "█", stlnc = "▀" }
-vim.opt.rulerformat = "%=%{%v:lua.GetErrorFlag()%}"
-vim.opt.cmdheight = 0
---up    ▀
---down  ▄
---full  █
---left  ▌
---right ▐
---
--- utils: nvim_win_get_position, fillchars
-
-vim.api.nvim_create_autocmd({ "WinEnter", "VimEnter" }, {
-	-- TODO exclude netrw somehow
-	callback = function(_)
-		vim.opt_local.colorcolumn = "80"
-		vim.opt_local.cursorcolumn = true
-		vim.opt_local.cursorline = true
-	end,
-})
---
-vim.api.nvim_create_autocmd({ "WinLeave" }, {
-	callback = function(_)
-		vim.opt_local.colorcolumn = ""
-		vim.opt_local.cursorcolumn = false
-		vim.opt_local.cursorline = false
-	end,
-})
