62dadcc2675ef1d1f4de06d0f90e5dd38c730e5c
alunturner <alun.turner@googlemail.com>
2024-06-04 20:32:13 +0100

    remove cursorcolumn from netrw

--- nvim/.config/nvim/after/ftplugin/netrw.lua
+++ nvim/.config/nvim/after/ftplugin/netrw.lua
@@ -1,5 +1,6 @@
 vim.g.netrw_banner = 0
 vim.opt_local.colorcolumn = ""
+vim.opt_local.cursorcolumn = false
 vim.keymap.set("n", "h", "-", { remap = true, buffer = true })
 vim.keymap.set("n", "l", "<cr>", { remap = true, buffer = true })
 vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { remap = true, buffer = true })
