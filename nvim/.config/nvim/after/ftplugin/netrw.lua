ae11c2b9f0ba1ce17ac345478cebd3f38a86f0f1
alunturner <alun.turner@googlemail.com>
2024-06-23 19:18:03 +0100

    update netrw to quit with <C-q>

--- nvim/.config/nvim/after/ftplugin/netrw.lua
+++ nvim/.config/nvim/after/ftplugin/netrw.lua
@@ -4,3 +4,4 @@ vim.opt_local.cursorcolumn = false
 vim.keymap.set("n", "h", "-", { remap = true, buffer = true })
 vim.keymap.set("n", "l", "<cr>", { remap = true, buffer = true })
 vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { remap = true, buffer = true })
+vim.keymap.set("n", "<C-q>", "<cmd>Rex<cr>", { remap = true, buffer = true })
