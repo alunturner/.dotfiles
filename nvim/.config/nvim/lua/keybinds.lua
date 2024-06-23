f7849f5ffad3e4bd3b7d9adc278d1c0a55019c32
alunturner <56027671+alunturner@users.noreply.github.com>
2024-06-23 21:08:36 +0100

    Post germany sweepup (#63)
    * delete ts text objects
    
    * delete custom text objects
    
    * close qf list on list clear
    
    * remove C-q mapping, very little time saved, open qf at window top
    
    * add a general C-q mapping
    
    * remove the J keymap
    
    * add comment
    
    * simplify la and ld
    
    * update lD
    
    * tidy up all of the list logic

--- nvim/.config/nvim/lua/keybinds.lua
+++ nvim/.config/nvim/lua/keybinds.lua
@@ -1,96 +1,78 @@
 vim.keymap.set("n", "<Esc><Esc>", "<cmd>noh<cr>", { silent = true }) -- Remove search hls
-vim.keymap.set("n", "J", "mzJ`z", { silent = true }) -- Don't move the cursor when using J
 vim.keymap.set("n", "<leader>e", "<cmd>Ex<cr>", { silent = true })
 -- can be removed when https://github.com/neovim/neovim/pull/29067 available
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { silent = true })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { silent = true })
 vim.keymap.set("i", "<C-j>", "<C-x><C-o>", { silent = true }) -- Lsp completion
 vim.keymap.set("i", "<C-f>", "<C-x><C-f>", { silent = true }) -- Filepath completion
+vim.keymap.set("n", "<C-q>", "<cmd>close<cr>", { silent = true }) -- General purpose quit
 
--- List add
+-- list add - add a file and increment the pointer
 vim.keymap.set("n", "<leader>la", function()
-	-- add a file and increment the pointer
-	pcall(vim.cmd, "argadd | next")
-
-	-- get the position and show status message
-	local arg_count = vim.fn.argc()
-	local arg_idx = vim.fn.argidx()
-	local one_based_idx = arg_idx + 1
-	local location = string.format("[%i|%i]", one_based_idx, arg_count)
-	local message = string.format("[A]%s File added to end of args list", location)
-	vim.print(message)
+	if pcall(vim.cmd, "argadd | next | argdedupe") then
+		print("File added to: [ARG]")
+	end
 end, { silent = true })
--- delete from args list and point at the previous arg or the beginning of the list
+
+-- list delete -  remove a file and and point at prev file (or list start)
 vim.keymap.set("n", "<leader>ld", function()
-	-- remove the file and decrement pointer
 	local first_or_prev = vim.fn.argidx() == 0 and "first" or "prev"
 	local command = string.format("argdelete %% | %s", first_or_prev)
-	pcall(vim.cmd, command)
-
-	-- get the position and show status message
-	local arg_count = vim.fn.argc()
-	local arg_idx = vim.fn.argidx()
-	local one_based_idx = arg_idx + 1
-
-	local location = string.format("[%i|%i]", one_based_idx, arg_count)
-	local message = arg_count == 0 and "[A] Args list empty"
-		or string.format("[A]%s File removed from args list", location)
-	vim.print(message)
-end, { silent = true })
--- Clear the qflist or argslist
-vim.keymap.set("n", "<leader>lD", function()
-	local qf = vim.fn.getqflist({ size = 0, idx = 0 })
-	local arg_count = vim.fn.argc()
-
-	local command = ""
-
-	if qf.size > 0 then
-		command = "call setqflist([]) | echo 'Qf list cleared'"
-	elseif arg_count > 0 then
-		command = "%argdelete | echo 'Args list cleared'"
-	else
-		command = "echo 'No lists to clear'"
+	if pcall(vim.cmd, command) then
+		print("File removed from: [ARG]")
 	end
-
-	pcall(vim.cmd, command)
 end, { silent = true })
--- TODO tidy up the logic and status messages, FIX this clobbers <C-i> and will
--- always do so in tmux. May need to move this to another key. I like being able
--- to trigger it with a single press though. Perhaps S-Tab could work.
+
+-- list Delete all - empties qflist first, then arglist
+vim.keymap.set("n", "<leader>lD", function()
+	if vim.fn.getqflist({ size = 0, idx = 0 }).size > 0 then
+		if pcall(vim.cmd, "call setqflist([]) | cclose") then
+			print("All entries removed from: [QF]")
+		end
+	elseif vim.fn.argc() > 0 then
+		if pcall(vim.cmd, "%argdelete") then
+			print("All entries removed from: [ARG]")
+		end
+	else
+		print("No lists to clear")
+	end
+end, { silent = true })
+
+-- FIX this clobbers <C-i> and will always do so in tmux. May need to move this
+-- to another key. I like being able to trigger it with a single press though.
+-- Perhaps S-Tab could work.
 -- Rotate through qflist, argslist or alt file
 vim.keymap.set("n", "<Tab>", function()
-	local qf = vim.fn.getqflist({ size = 0, idx = 0 })
-	local arg_count = vim.fn.argc()
-	local arg_idx = vim.fn.argidx()
+	local initial_arg_count = vim.fn.argc()
+	local initial_qf = vim.fn.getqflist({ size = 0, idx = 0 })
 
-	local command = ""
-	local message = ""
-
-	if qf.size > 0 then
-		command = qf.idx == qf.size and "cfirst" or "cnext"
-	elseif arg_count > 0 then
-		command = arg_idx == arg_count - 1 and "first | args" or "next | args"
+	if initial_qf.size > 0 then
+		if pcall(vim.cmd, initial_qf.idx == initial_qf.size and "cfirst" or "cnext") then
+			local current_qf = vim.fn.getqflist({ size = 0, idx = 0 })
+			print("[QF] " .. current_qf.idx .. " of " .. current_qf.size)
+		end
+	elseif initial_arg_count > 0 then
+		if pcall(vim.cmd, vim.fn.argidx() == initial_arg_count - 1 and "first" or "next") then
+			print("[ARG] " .. vim.fn.argidx() + 1 .. " of " .. vim.fn.argc())
+		end
 	else
-		command = "e #" -- equivalent of <C-^>
+		pcall(vim.cmd, "e #") -- can have errors if no alt file
 	end
-
-	pcall(vim.cmd, command) -- can have errors if no alt file
 end, { silent = true })
--- Rotate backwards through qflist, argslist or alt file
+
+-- Rotate backwards through qflist or arglist
 vim.keymap.set("n", "<S-Tab>", function()
-	local qf = vim.fn.getqflist({ size = 0, idx = 0 })
-	local arg_count = vim.fn.argc()
-	local arg_idx = vim.fn.argidx()
+	local initial_arg_count = vim.fn.argc()
+	local initial_qf = vim.fn.getqflist({ size = 0, idx = 0 })
 
-	local command = ""
-
-	if qf.size > 0 then
-		command = qf.idx == 1 and "clast" or "cprev"
-	elseif arg_count > 0 then
-		command = arg_idx == 0 and "last | args" or "prev | args"
-	else
-		command = "e #" -- equivalent of <C-^>
+	if initial_qf.size > 0 then
+		if pcall(vim.cmd, initial_qf.idx == 1 and "clast" or "cprev") then
+			local current_qf = vim.fn.getqflist({ size = 0, idx = 0 })
+			print("[QF] " .. current_qf.idx .. " of " .. current_qf.size)
+		end
+	elseif initial_arg_count > 0 then
+		if pcall(vim.cmd, vim.fn.argidx() == 0 and "last" or "prev") then
+			print("[ARG] " .. vim.fn.argidx() + 1 .. " of " .. vim.fn.argc())
+		end
 	end
-
-	pcall(vim.cmd, command) -- can have errors if no alt file
 end, { silent = true })
