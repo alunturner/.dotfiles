126d250400f3c3b034153db379dab692a9105fbb
alunturner <alun.turner@googlemail.com>
2024-06-23 20:16:01 +0100

    remove the J keymap

--- nvim/.config/nvim/lua/keybinds.lua
+++ nvim/.config/nvim/lua/keybinds.lua
@@ -1,5 +1,4 @@
 vim.keymap.set("n", "<Esc><Esc>", "<cmd>noh<cr>", { silent = true }) -- Remove search hls
-vim.keymap.set("n", "J", "mzJ`z", { silent = true }) -- Don't move the cursor when using J
 vim.keymap.set("n", "<leader>e", "<cmd>Ex<cr>", { silent = true })
 -- can be removed when https://github.com/neovim/neovim/pull/29067 available
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { silent = true })
