7a56a67e38d5112e985b1011a49aee09dd71b2a1
alunturner <56027671+alunturner@users.noreply.github.com>
2024-06-30 15:33:26 +0100

    Swap tab behaviour onto number keys (#66)
    * add <C-p> to the omnifunc call
    
    * add error filter to diagnostic movement
    
    * add a build binding
    
    * remove the magic tab

--- nvim/.config/nvim/lua/keybinds.lua
+++ nvim/.config/nvim/lua/keybinds.lua
@@ -1,78 +1,82 @@
 vim.keymap.set("n", "<Esc><Esc>", "<cmd>noh<cr>", { silent = true }) -- Remove search hls
 vim.keymap.set("n", "<leader>e", "<cmd>Ex<cr>", { silent = true })
--- can be removed when https://github.com/neovim/neovim/pull/29067 available
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { silent = true })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { silent = true })
-vim.keymap.set("i", "<C-j>", "<C-x><C-o>", { silent = true }) -- Lsp completion
-vim.keymap.set("i", "<C-f>", "<C-x><C-f>", { silent = true }) -- Filepath completion
-vim.keymap.set("n", "<C-q>", "<cmd>close<cr>", { silent = true }) -- General purpose quit
+vim.keymap.set("n", "[d", function()
+	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
+end, { silent = true })
+vim.keymap.set("n", "]d", function()
+	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
+end, { silent = true })
+vim.keymap.set("n", "3", function()
+	local qf_start = vim.fn.getqflist({ idx = 0, size = 0 })
+	if qf_start.size == 0 then
+		return print("[QF] empty")
+	end
 
--- list add - add a file and increment the pointer
-vim.keymap.set("n", "<leader>la", function()
+	if qf_start.idx == 1 then
+		return print("[QF] 1 of " .. qf_start.size)
+	end
+
+	vim.cmd("cprev")
+	local qf_end = vim.fn.getqflist({ idx = 0, size = 0 })
+	print("[QF] " .. qf_end.idx .. " of " .. qf_end.size)
+end)
+vim.keymap.set("n", "4", function()
+	local qf_start = vim.fn.getqflist({ idx = 0, size = 0 })
+	if qf_start.size == 0 then
+		return print("[QF] empty")
+	end
+
+	if qf_start.idx == qf_start.size then
+		return print("[QF] " .. qf_start.idx .. " of " .. qf_start.size)
+	end
+
+	vim.cmd("cnext")
+	local qf_end = vim.fn.getqflist({ idx = 0, size = 0 })
+	print("[QF] " .. qf_end.idx .. " of " .. qf_end.size)
+end)
+vim.keymap.set("n", "6", "<C-6>", { silent = true })
+vim.keymap.set("n", "7", function()
+	if vim.fn.argc() == 0 then
+		return print("[ARG] empty")
+	end
+
+	if vim.fn.argidx() == 0 then
+		return print("[ARG] 1 of " .. vim.fn.argc())
+	end
+
+	vim.cmd("prev")
+	print("[ARG] " .. vim.fn.argidx() + 1 .. " of " .. vim.fn.argc())
+end)
+vim.keymap.set("n", "8", function()
+	if vim.fn.argc() == 0 then
+		return print("[ARG] empty")
+	end
+
+	if vim.fn.argidx() == vim.fn.argc() - 1 then
+		return print("[ARG] " .. vim.fn.argidx() + 1 .. " of " .. vim.fn.argc())
+	end
+
+	vim.cmd("next")
+	print("[ARG] " .. vim.fn.argidx() + 1 .. " of " .. vim.fn.argc())
+end)
+-- arglist add - add a file and increment the pointer
+vim.keymap.set("n", "9", function()
 	if pcall(vim.cmd, "argadd | next | argdedupe") then
 		print("File added to: [ARG]")
 	end
 end, { silent = true })
-
--- list delete -  remove a file and and point at prev file (or list start)
-vim.keymap.set("n", "<leader>ld", function()
+-- arglist delete -  remove a file and and point at prev file (or list start)
+vim.keymap.set("n", "0", function()
 	local first_or_prev = vim.fn.argidx() == 0 and "first" or "prev"
 	local command = string.format("argdelete %% | %s", first_or_prev)
 	if pcall(vim.cmd, command) then
 		print("File removed from: [ARG]")
 	end
 end, { silent = true })
-
--- list Delete all - empties qflist first, then arglist
-vim.keymap.set("n", "<leader>lD", function()
-	if vim.fn.getqflist({ size = 0, idx = 0 }).size > 0 then
-		if pcall(vim.cmd, "call setqflist([]) | cclose") then
-			print("All entries removed from: [QF]")
-		end
-	elseif vim.fn.argc() > 0 then
-		if pcall(vim.cmd, "%argdelete") then
-			print("All entries removed from: [ARG]")
-		end
-	else
-		print("No lists to clear")
-	end
-end, { silent = true })
-
--- FIX this clobbers <C-i> and will always do so in tmux. May need to move this
--- to another key. I like being able to trigger it with a single press though.
--- Perhaps S-Tab could work.
--- Rotate through qflist, argslist or alt file
-vim.keymap.set("n", "<Tab>", function()
-	local initial_arg_count = vim.fn.argc()
-	local initial_qf = vim.fn.getqflist({ size = 0, idx = 0 })
-
-	if initial_qf.size > 0 then
-		if pcall(vim.cmd, initial_qf.idx == initial_qf.size and "cfirst" or "cnext") then
-			local current_qf = vim.fn.getqflist({ size = 0, idx = 0 })
-			print("[QF] " .. current_qf.idx .. " of " .. current_qf.size)
-		end
-	elseif initial_arg_count > 0 then
-		if pcall(vim.cmd, vim.fn.argidx() == initial_arg_count - 1 and "first" or "next") then
-			print("[ARG] " .. vim.fn.argidx() + 1 .. " of " .. vim.fn.argc())
-		end
-	else
-		pcall(vim.cmd, "e #") -- can have errors if no alt file
-	end
-end, { silent = true })
-
--- Rotate backwards through qflist or arglist
-vim.keymap.set("n", "<S-Tab>", function()
-	local initial_arg_count = vim.fn.argc()
-	local initial_qf = vim.fn.getqflist({ size = 0, idx = 0 })
-
-	if initial_qf.size > 0 then
-		if pcall(vim.cmd, initial_qf.idx == 1 and "clast" or "cprev") then
-			local current_qf = vim.fn.getqflist({ size = 0, idx = 0 })
-			print("[QF] " .. current_qf.idx .. " of " .. current_qf.size)
-		end
-	elseif initial_arg_count > 0 then
-		if pcall(vim.cmd, vim.fn.argidx() == 0 and "last" or "prev") then
-			print("[ARG] " .. vim.fn.argidx() + 1 .. " of " .. vim.fn.argc())
-		end
-	end
-end, { silent = true })
+vim.keymap.set("i", "<C-j>", "<C-x><C-o><C-p>", { silent = true }) -- Lsp completion
+vim.keymap.set("n", "<C-q>", "<cmd>close<cr>", { silent = true }) -- General purpose quit
+vim.keymap.set(
+	"n",
+	"<leader>m",
+	"<cmd>compiler tsc | echo 'Building TypeScript...' | silent make! --noEmit | echo 'TypeScript built.' | copen<cr>"
+)
