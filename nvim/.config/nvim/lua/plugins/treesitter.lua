4c2d23a201f20927156a0f999c3b798cf4c8da27
alunturner <alun.turner@googlemail.com>
2024-09-17 18:12:41 +0100

    try and unbreak treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 local parsers = { "css", "javascript", "lua", "typescript", "tsx", "vim", "vimdoc" }
 
-require("nvim-treesitter").configs.setup({
+require("nvim-treesitter.configs").setup({
 	ensure_installed = parsers,
 	sync_install = false,
 	auto_install = false,
