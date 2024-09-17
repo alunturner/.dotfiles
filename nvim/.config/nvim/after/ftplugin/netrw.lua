6741a4d19404a8dcc5ea6057b5e1398264d88a8a
alunturner <alun.turner@googlemail.com>
2024-09-17 16:49:55 +0100

    use a function to keep it updated in netrw

--- nvim/.config/nvim/after/ftplugin/netrw.lua
+++ nvim/.config/nvim/after/ftplugin/netrw.lua
@@ -5,7 +5,10 @@ vim.g.netrw_altfile = 1 -- make <C-6> go back to prev file, not netrw
 vim.g.netrw_localcopydircmd = "cp -r" -- allow whole file copying
 vim.opt_local.colorcolumn = ""
 vim.opt_local.cursorcolumn = false
-vim.opt_local.winbar = "hello"
+function NetrwWinBar()
+	return "%#Normal#  %t %*%=%#Normal# 󰋞 " .. vim.fn.getcwd() .. " "
+end
+vim.opt_local.winbar = "%{%v:lua.NetrwWinBar()%}"
 vim.keymap.set("n", "h", "-", { remap = true, buffer = true })
 vim.keymap.set("n", "l", "<cr>", { remap = true, buffer = true })
 vim.keymap.set("n", "dd", "D", { remap = true, buffer = true })
