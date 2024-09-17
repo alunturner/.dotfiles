03fe6348d3543fae07b7d36cdfffa7796bc94d81
alunturner <alun.turner@googlemail.com>
2024-09-17 17:27:19 +0100

    allow C-n and C-p to traverse qf list without entering window

--- nvim/.config/nvim/after/ftplugin/qf.lua
+++ nvim/.config/nvim/after/ftplugin/qf.lua
@@ -1,3 +1,5 @@
 vim.opt_local.colorcolumn = ""
 vim.opt_local.cursorcolumn = false
 vim.cmd("wincmd K")
+vim.keymap.set("n", "<C-n>", "<cmd>cnext | wincmd p<cr>", { remap = true, buffer = true })
+vim.keymap.set("n", "<C-p>", "<cmd>cprev | wincmd p<cr>", { remap = true, buffer = true })
