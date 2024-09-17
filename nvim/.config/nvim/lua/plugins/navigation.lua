f6c6552ed39c5cfa9b7b9203f9ee61e4825ea304
alunturner <alun.turner@googlemail.com>
2024-09-17 18:47:15 +0100

    move all the smaller configs together

--- nvim/.config/nvim/lua/plugins/navigation.lua
+++
@@ -1,5 +0,0 @@
-vim.g.tmux_navigator_no_wrap = 1
-vim.keymap.set("n", "<c-h>", "<cmd>TmuxNavigateLeft<cr>")
-vim.keymap.set("n", "<c-j>", "<cmd>TmuxNavigateDown<cr>")
-vim.keymap.set("n", "<c-k>", "<cmd>TmuxNavigateUp<cr>")
-vim.keymap.set("n", "<c-l>", "<cmd>TmuxNavigateRight<cr>")
