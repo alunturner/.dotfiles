0421a03092620605616f9bd2b94d681f7ee528e4
alunturner <alun.turner@googlemail.com>
2024-09-17 17:51:26 +0100

    remove lazy from init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,22 +1,8 @@
--- PLUGIN MANAGER
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.keymap.set({ "n", "v" }, " ", "<nop>", { silent = true })
 
--- PLUGINS
-require("lazy").setup("plugins", { ui = { border = "rounded" } })
+-- PLUGIN CONFIGURATION
 
 -- KEYBINDS
 vim.keymap.set("n", "<Esc><Esc>", "<cmd>noh<cr>", { silent = true })
