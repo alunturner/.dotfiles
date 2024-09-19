c78e26353412c146deb1768fa90c02b8dd1266f8
alunturner <56027671+alunturner@users.noreply.github.com>
2024-09-19 21:23:18 +0100

    Add []d back in and go back to unnamedplus (#79)
    * add unnamedplus clipboard option back in
    
    * add the next/prev diagnostic binds back in

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -11,6 +11,8 @@ require("plugin_config.other")
 local ERROR = { severity = vim.diagnostic.severity.ERROR }
 vim.keymap.set("n", "<Esc><Esc>", "<cmd>noh<cr>", { silent = true })
 vim.keymap.set("n", "<leader>e", "<cmd>Ex<cr>", { silent = true })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { silent = true })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { silent = true })
 vim.keymap.set("n", "[e", function()
 	vim.diagnostic.goto_prev(ERROR)
 end, { silent = true })
@@ -30,6 +32,7 @@ end, {})
 -- OPTIONS
 vim.o.guicursor = vim.o.guicursor .. ",a:Cursor" -- append hl-Cursor to all modes
 vim.opt.breakindent = true
+vim.opt.clipboard = "unnamedplus"
 vim.opt.colorcolumn = "80"
 vim.opt.cursorcolumn = true
 vim.opt.cursorline = true
